edfa0213c35361c4e28a2d113abefdc8ccd5cf6a
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2024-12-15 23:05:12 +0530

    DEPLOYING FRONTEND

--- FRONTEND/src/environment.js
+++ FRONTEND/src/environment.js
@@ -1,6 +1,8 @@
 let IS_PROD = true;
-const server = IS_PROD
-  ? "https://videocallbackend-uxie.onrender.com/"
-  : "http://localhost:8000";
+const server = IS_PROD ?
+    "https://apnacollegebackend.onrender.com" :
+
+    "http://localhost:8000"
+
 
 export default server;
